fd187b36fd450666f89147309b45af90d83a90ae
Yoni Asleno Farid <92686606+aslenofarid@users.noreply.github.com>
2023-08-03 22:25:03 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -14,8 +14,4 @@ git config --global user.name $GITHUB_USERNAME
 git config --global user.email $GITHUB_EMAIL
 git clone --recursive $KERNEL_REPO -b $BRANCH kernel
 cd kernel
-git remote add dms https://github.com/dimas-ady/kernel_asus_sdm660
-git fetch dms
-curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
-git cherry-pick b175639^..1b51943
 bash ../build.sh
